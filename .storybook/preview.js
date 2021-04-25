84b73cf5915b90b40c8d2356e338553a3670544e
Zé Yuri <jozeyuri@gmail.com>
2021-04-25 09:58:13 -0300

    build(msw): add msw worker to storybook preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,4 @@
+import { worker } from "../src/mocks/browser"
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -7,4 +8,11 @@ export const parameters = {
       date: /Date$/,
     },
   },
+}
+
+if (typeof global.process === "undefined") {
+  // Start the mocking when each story is loaded.
+  // Repetitive calls to the `.start()` method do not register a new worker,
+  // but check whether there's an existing once, reusing it, if so.
+  worker.start()
 }
